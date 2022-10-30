70c762e30b02fdc7bb933cfe0fb0df1111ff602f
NirjoyDebnath <97026247+NirjoyDebnath@users.noreply.github.com>
2022-10-30 18:28:32 +0600

    Create A.cpp

---
+++ Codeforces Round #831 (Div. 1 + Div. 2)/A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(v) (v).begin(),(v).end()
+#define memset(v,x) memset((v),x,(sizeof(v)))
+using namespace std;
+typedef long long int lli;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    lli t,n,i,x,mx,mn,f,j,s,r,y,c,m,k;
+    cin>>t;
+    while(t--)
+    {
+        cin>>x;
+        cout<<7<<endl;
+    }
+}
